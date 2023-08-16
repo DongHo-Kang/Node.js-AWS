189326c635eb9b86cbb6650f2475c83b03b4e3cb
DongHo-Kang <gkqjem981113@gmail.com>
2023-08-16 12:19:22 +0900

    데이터베이스 접속 정보 수정

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const controller = require("../controller/Cmain.js");
 const router = express.Router();
+const controller = require("./controller/CVisitor");
 
-router.get("/", controller.main); //GET /
-router.get("/comments", controller.comments); // GET /comments
-router.get("/comment/:id", controller.comment); //GET/comment/:id
+//방명록 전체 보이기
+//localhost:8000/visitor
+router.get("/", controller.getVisitors);
 
-module.exports = router;
+//방명록 하나 조회
+//localhost:8000/visitor/get
+router.get("/get", controller.getVisitor);
 
-//경로를 controller와 연결해 설정 가능
+//방명록 하나 추가
+router.post("/write", controller.postVisitor);
+
+//방명록 하나 수정
+router.patch("/edit", controller.patchVisitor);
+
+//방명록 하나 삭제
+router.delete("/delete", controller.deleteVisitor);
+
+module.exports = router; //외부로 라우터를 보낸다.
